4f6c94a8ea1652d67504d5960e1453e8033c9bf8
Aphaki <kiapha1108@gmail.com>
2022-04-25 14:59:27 +0900

    Initial Commit

---
+++ MyStopWatch/MyStopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStopWatchApp.swift
+//  MyStopWatch
+//
+//  Created by Sy Lee on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyStopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
